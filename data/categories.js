e51e1d01eb0aeb019a9beb3abac71b7b53126bed
AhmadAkhdan <AhmadAkhdan1705@gmail.com>
2025-10-16 13:05:42 +0700

    GANTI FOTO KATEGORI

--- data/categories.js
+++ data/categories.js
@@ -1,7 +1,7 @@
 export const categories = [
   { id: 1, name: "Nasi", image: "/images/nasi.jpg" },     
   { id: 2, name: "Soto", image: "/images/soto.jpg" },
-  { id: 3, name: "Sate", image: "/images/sate.jpg" },
+  { id: 3, name: "Sate", image: "/images/sate-ayam.jpeg" },
   { id: 4, name: "Seafood", image: "/images/seafood.jpg" },
   { id: 5, name: "Jajanan Pasar", image: "/images/jajanan-pasar.jpg" },
   { id: 6, name: "Minuman", image: "/images/minuman.jpg" },
